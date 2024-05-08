a09ff1072a2498a1ef1e83bd2f9b6dd5476e0339
ivanjameslo <115435118+ivanjameslo@users.noreply.github.com>
2024-05-08 21:49:42 +0800

    Add files via upload

---
+++ myaddons/hotel/models/rooms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+class room(models.Model):
+    _name = 'hotel.rooms'
+    _description = 'Hotel Rooms'
+    _order = 'name'
+
+    name = fields.Char("Room No.")
+    description = fields.Char("Room Description")
+    roomtype_id=fields.Many2one('hotel.roomtypes', string="Room Type")
+    roomtypename=fields.Char("Room Type", related='roomtype_id.name')
